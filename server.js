604c727b42afea60b8cf90082a56799cc4e0817a
stvnfrlls <stvnfrlls@gmail.com>
2022-09-26 02:19:51 +0800

    Initial Setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app     = express();
+
+const indexRouter = require('./routes/index');
+const accountsRouter = require('./routes/accounts');
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.use('/', indexRouter);
+app.use('/accounts', accountsRouter);
+
+app.listen(process.env.PORT || 3000);
